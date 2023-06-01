7674783a6c65c3ede47a89f5aed78dc5b561094c
Ted2121 <andrei.teodor1372@gmail.com>
2023-06-01 16:14:27 +0200

    Added Question repository interface

---
+++ QuizBytes2Solution/QuizBytes2/Data/IQuestionRepository.cs
@@ -0,0 +1,14 @@
+﻿using QuizBytes2.Models;
+using System.Linq.Expressions;
+
+namespace QuizBytes2.Data;
+
+public interface IQuestionRepository
+{
+    Task<IEnumerable<Question>> GetAllQuestionsAsync();
+    Task<IEnumerable<Question>> GetQuestionByIdAsync();
+    Task<IEnumerable<Question>> GetQuestionsAsync(Expression<Func<Question, bool>> predicate);
+    Task<string> CreateQuestionAsync(Question question);
+    Task<bool> UpdateQuestionAsync(Question question);
+    Task<bool> DeleteQuestionAsync(string id);
+}
---
+++ QuizBytes2Solution/QuizBytes2/Data/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using QuizBytes2.Models;
+using System.Linq.Expressions;
+
+namespace QuizBytes2.Data;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetAllUsersAsync();
+    Task<IEnumerable<User>> GetUsersAsync(Expression<Func<User, bool>> predicate);
+    Task<User> GetUserByIdAsync(string id);
+    Task<string> CreateUserAsync(User user);
+    Task<bool> UpdateUserAsync(User user);
+    Task<bool> DeleteUserAsync(string id);
+    User Login(string username, string password);
+    Task<bool> UpdatePasswordAsync(string username, string oldPassword, string newPassword);
+    Task<bool> UpdateLastQuizByUserIdAsync(string id, LastQuizResult lastQuizResult);
+    Task<LastQuizResult> GetLastQuizByUserIdAsync(string id);
+}
